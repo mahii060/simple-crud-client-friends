1510ee63fc8e1ef53047798f34a5c2bc40dbea0f
mahii060 <mdmahebul-27-2020518110@ier.du.ac.bd>
2025-04-15 00:57:06 +0600

    Successfully implemented PUT http request

--- src/components/Update.jsx
+++ src/components/Update.jsx
@@ -1,11 +1,40 @@
 import { useLoaderData } from "react-router-dom";
 
 const Update = () => {
-    const friend = useLoaderData()
-    console.log(friend);
+    const loadedFriend = useLoaderData()
+    console.log(loadedFriend);
+    const handleUpdate = (event) => {
+        event.preventDefault();
+        const form = event.target;
+        const name = form.name.value;
+        const department = form.department.value;
+        const updatedFriend = { name, department }
+        fetch(`http://localhost:5000/friends/${loadedFriend._id}`, {
+            method: "PUT",
+            headers: {
+                "content-type": "application/json"
+            },
+            body: JSON.stringify(updatedFriend)
+        })
+            .then(res => res.json())
+            .then(data => {
+                if (data.modifiedCount > 0) {
+                    alert(`${loadedFriend.name} is updated successfully!`)
+                }
+                console.log(data);
+            })
+
+    }
     return (
         <div>
-            <h2>This is update page: {friend.name}</h2>
+            <h2>This is update page: {loadedFriend.name}</h2>
+            <form onSubmit={handleUpdate}>
+                <input type="text" name="name" id="" defaultValue={loadedFriend.name} />
+                <br />
+                <input type="text" name="department" id="" defaultValue={loadedFriend.department} />
+                <br />
+                <input type="submit" value="Update Friend" />
+            </form>
         </div>
     );
 };
